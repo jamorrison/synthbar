cd7d2ed4b5fa53b379a98d36bc3f62bd91e87cda
Jacob Morrison <Jacob.Morrison@vai.org>
2022-10-24 09:22:10 -0400

    Some minor refactoring

--- synthbar.c
+++ synthbar.c
@@ -3,37 +3,35 @@
  *
  * Copyright (c) 2022 Jacob Morrison <jacob.morrison@vai.org>
  *
- * Permission is hereby granted, free of charge, to any person obtaining
- * a copy of this software and associated documentation files (the
- * "Software"), to deal in the Software without restriction, including
- * without limitation the rights to use, copy, modify, merge, publish,
- * distribute, sublicense, and/or sell copies of the Software, and to
- * permit persons to whom the Software is furnished to do so, subject to
- * the following conditions:
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
- * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
- * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
- *
  */
 #include <stdio.h>
 #include <stdint.h>
-#include <zlib.h>
 #include <getopt.h>
 #include <sys/time.h>
+#include <zlib.h>
 
 #include "kseq.h"
 KSEQ_INIT(gzFile, gzread)
 
 // What the function name says!
+// Returns time in seconds
 static inline double get_current_time() {
     struct timeval  tp;
     struct timezone tzp;
@@ -110,7 +108,10 @@ int main(int argc, char *argv[]) {
     };
 
     // Parse CLI
-    if (argc < 2) { usage(&conf); return 0; }
+    if (argc < 2) {
+        usage(&conf);
+        return 0;
+    }
     while ((c = getopt_long(argc, argv, "l:o:u:hrz", loptions, NULL)) >= 0) {
         switch (c) {
             case 'o':
@@ -127,7 +128,7 @@ int main(int argc, char *argv[]) {
                 break;
             case 'h':
                 usage(&conf);
-                return 1;
+                return 0;
             case 1:
                 print_version();
                 return 0;
@@ -150,16 +151,19 @@ int main(int argc, char *argv[]) {
     FILE   *oh1 = strcmp(conf.outfn, "-")  == 0 ? stdout : fopen(conf.outfn, "w");
     kseq_t *ks1 = kseq_init(fh1);
 
-    uint16_t total_length = conf.umi_length + conf.linker_length;
+    uint16_t u_plus_l   = conf.umi_length + conf.linker_length;
+    uint32_t read_count = 0;
+
     double t1 = get_current_time();
     while (kseq_read(ks1) >= 0) {
+        read_count++;
+
         fprintf(oh1, "%s %s\nCATATAC", ks1->name.s, ks1->comment.s);
 
         if (conf.remove_linker) {
             // Handle error case, seq and qual should be same length, so only check seq
-            if (ks1->seq.l < conf.umi_length+conf.linker_length) {
-                fprintf(stderr, "Read not long enough for UMI and linker lengths provided (%li<%u)\n",
-                        ks1->seq.l, conf.umi_length+conf.linker_length);
+            if (ks1->seq.l < u_plus_l) {
+                fprintf(stderr, "Read shorter than UMI and linker lengths provided (%li<%u)\n", ks1->seq.l, u_plus_l);
                 kseq_destroy(ks1);
                 if (strcmp(conf.outfn, "-") != 0) { fclose(oh1); }
                 gzclose(fh1);
@@ -168,49 +172,14 @@ int main(int argc, char *argv[]) {
             }
 
             fprintf(oh1, "%.*s", conf.umi_length, ks1->seq.s);
-            fprintf(oh1, "%s\n+\nIIIIIII", ks1->seq.s + total_length);
+            fprintf(oh1, "%s\n+\nIIIIIII", ks1->seq.s + u_plus_l);
 
             fprintf(oh1, "%.*s", conf.umi_length, ks1->qual.s);
-            fprintf(oh1, "%s\n", ks1->qual.s + total_length);
+            fprintf(oh1, "%s\n", ks1->qual.s + u_plus_l);
         } else {
             fprintf(oh1, "%s\n+\nIIIIIII%s\n", ks1->seq.s, ks1->qual.s);
         }
-        //fprintf(oh1, "%s %s\nCATATAC%s\n+\nIIIIIII%s\n", ks1->name.s, ks1->comment.s, ks1->seq.s, ks1->qual.s);
     }
-    //while (kseq_read(ks1) >= 0) {
-    //    ksprintf(&str, "%s %s\nCATATAC", ks1->name.s, ks1->comment.s);
-
-    //    // Create sequence and quality strings without linker if desired
-    //    if (conf.remove_linker) {
-    //        // Handle error case, seq and qual should be same length, so only check seq
-    //        if (ks1->seq.l < conf.umi_length+conf.linker_length) {
-    //            fprintf(stderr, "Read not long enough for UMI and linker lengths provided (%li<%u)\n",
-    //                    ks1->seq.l, conf.umi_length+conf.linker_length);
-    //            kseq_destroy(ks1);
-    //            if (strcmp(conf.outfn, "-") != 0) { fclose(oh1); }
-    //            gzclose(fh1);
-
-    //            return 1;
-    //        }
-
-    //        // Set up seq string
-    //        kputsn(ks1->seq.s, conf.umi_length, &str);
-    //        kputsn(ks1->seq.s + total_length, ks1->seq.l - total_length, &str);
-
-    //        ksprintf(&str, "\n+\nIIIIIII");
-
-    //        // Set up qual string
-    //        kputsn(ks1->qual.s, conf.umi_length, &str);
-    //        kputsn(ks1->qual.s + total_length, ks1->qual.l - total_length, &str);
-    //    } else {
-    //        ksprintf(&str, "%s\n+\nIIIIIII%s", ks1->seq.s, ks1->qual.s);
-    //    }
-
-    //    //fprintf(oh1, "%s %s\nCATATAC%s\n+\nIIIIIII%s\n", ks1->name.s, ks1->comment.s, ks1->seq.s, ks1->qual.s);
-    //    fprintf(oh1, "%s\n", str.s);
-
-    //    ks_release(&str);
-    //}
     double t2 = get_current_time();
 
     // Clean up
@@ -218,7 +187,7 @@ int main(int argc, char *argv[]) {
     if (strcmp(conf.outfn, "-") != 0) { fclose(oh1); }
     gzclose(fh1);
 
-    fprintf(stderr, "[synthbar:%s] Wall time: %.3f seconds\n", __func__, t2-t1);
+    fprintf(stderr, "[synthbar:%s] %u reads processed in %.3f seconds (wall time)\n", __func__, read_count, t2-t1);
 
     return 0;
 }
