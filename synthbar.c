f6ddd86607afa2dc8c7592f387b86203255fd3f6
Jacob Morrison <Jacob.Morrison@vai.org>
2023-05-10 11:57:50 -0400

    update copyright year

--- synthbar.c
+++ synthbar.c
@@ -1,7 +1,7 @@
 /*
  * The MIT License
  *
- * Copyright (c) 2022 Jacob Morrison <jacob.morrison@vai.org>
+ * Copyright (c) 2022-2023 Jacob Morrison <jacob.morrison@vai.org>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
